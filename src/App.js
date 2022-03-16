4cf6bbe8132a0ec705e3a684630c5d46a8343cea
Salohiddin <turgunovsaloxiddin018@gmail.com>
2022-03-16 15:42:31 +0500

    Infotable yordim

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import Cards from './Conponents/Cards/Cards';
-// import Items from './Conponents/Item/Items';
-// import Navbar from './Conponents/Navbar/Navbar_';
-// import Servis from './Conponents/Servis/Servis';
+ import Cards from './Conponents/Cards/Cards';
+ import Items from './Conponents/Item/Items';
+ import Navbar from './Conponents/Navbar/Navbar_';
+ import Servis from './Conponents/Servis/Servis';
 import Infotable from './Conponents/Infotable/Infotable';
 
 
 function App() {
   return (
     <>
+    <Navbar />
+    <Items />
+    <Servis />
     <Infotable />
     </>
   )
