093641076adf1cc6799534cd726e8a795c502e32
Salohiddin <turgunovsaloxiddin018@gmail.com>
2022-03-16 15:38:56 +0500

    Infotable yordim

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 // import Cards from './Conponents/Cards/Cards';
 // import Items from './Conponents/Item/Items';
 // import Navbar from './Conponents/Navbar/Navbar_';
-import Servis from './Conponents/Servis/Servis';
-
+// import Servis from './Conponents/Servis/Servis';
+import Infotable from './Conponents/Infotable/Infotable';
 
 
 function App() {
   return (
     <>
-      <Servis />
+    <Infotable />
     </>
   )
 }
