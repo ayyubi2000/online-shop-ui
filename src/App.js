bb75f4f12204770ebcc1d7d5834537be61c2ebbe
Saloxiddin <turgunovsaloxiddin018@gmail.com>
2022-03-16 19:23:39 +0500

    Navbar,Item,Filter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Cards from './Conponents/Cards/Cards';
+import Description from './Conponents/Description/Description';
 import Items from './Conponents/Item/Items';
 import Laptops from './Conponents/Laptops/Laptops';
 import Navbar from './Conponents/Navbar/Navbar_';
@@ -11,6 +12,7 @@ function App() {
      <Navbar/>
       <Items/>
       <Laptops/>
+      <Description/>
     </>
   )
 }
