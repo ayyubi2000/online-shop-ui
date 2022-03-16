ea12c89aa1969fb772c3fa02de1e18a24cbe5ce4
unknown <afzalbekholmatov2004@gmail.com>
2022-03-16 16:24:35 +0200

    Card yasadim

--- src/App.js
+++ src/App.js
@@ -2,20 +2,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 // import Cards from './Conponents/Cards/Cards';
 // import Items from './Conponents/Item/Items';
 // import Navbar from './Conponents/Navbar/Navbar_';
+//import LikeCard from './Conponents/Cards/LikeCard'
 
 
 function App() {
   return (
     <>
-      
+    
     </>
   )
 }
-
-
-
-
-
-
-
 export default App;
