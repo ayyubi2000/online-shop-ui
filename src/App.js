5e90066c26a0b0a0958178b443a8262aa34041e6
Navruzbek <navrozbekraximov@gmail.com>
2022-03-17 15:37:56 +0500

    carusel

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "boxicons/css/boxicons.css"
 // import Cards from './Conponents/Cards/Cards';
 // import Items from './Conponents/Item/Items';
 // import Navbar from './Conponents/Navbar/Navbar_';
-import FeatureProducts from './Conponents/fProducts/fProduct';
+// import FeatureProducts from './Conponents/fProducts/fProduct';
+import Carusel from './Conponents/Crousel/Carusel';
 
 function App() {
   return (
     <>
-      <FeatureProducts  />
+      {/* <FeatureProducts  /> */}
+      <Carusel />
     </>
   )
 }
