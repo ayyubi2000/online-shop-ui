3c3c4b5e45578fa7a99be468efa6cfb6e82a7555
Navruzbek <navrozbekraximov@gmail.com>
2022-03-18 16:20:05 +0500

    HelpMessage

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import "bootstrap/dist/css/bootstrap.min.css";
 // import Items from './Conponents/Item/Items';
 // import Navbar from './Conponents/Navbar/Navbar_';
 // import FeatureProducts from './Conponents/fProducts/fProduct';
-import Carusel from './Conponents/Crousel/Carusel';
+// import Carusel from './Conponents/Crousel/Carusel';
+import HelpMessage from "./Conponents/HelpMessage/HelpMessage";
 
 function App() {
   return (
     <>
+    <HelpMessage />
       {/* <FeatureProducts  /> */}
-      <Carusel />
+      {/* <Carusel /> */}
     </>
   );
 }
