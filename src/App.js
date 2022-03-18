4eea055f7581992b9dfdb4a6e6ebcabf9816b86c
Salohiddin <turgunovsaloxiddin018@gmail.com>
2022-03-18 14:48:51 +0500

    infotable tayyor

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
  import Cards from './Conponents/Cards/Cards';
  import Items from './Conponents/Item/Items';
- import Navbar from './Conponents/Navbar/Navbar_';
+ import Navbar from './Conponents/Navbar/Navbar';
  import Servis from './Conponents/Servis/Servis';
 import Infotable from './Conponents/Infotable/Infotable';
 import Laptops from './Conponents/Laptops/Laptops'
