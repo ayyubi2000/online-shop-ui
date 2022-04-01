563719e116819d05822328b104e228db60e8e06d
Abdulloh <valiyevvabdullohh@gmail.com>
2022-04-01 15:10:05 +0500

    pull qilishga commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { Routes, Route } from 'react-router-dom';
 import Main from "./Layout/Main/Main";
 import "./Assets/Style/Config.scss";
 import Home from "./Pages/Home/Home";
+import Laptops from './Conponents/Laptops/Laptops';
 
 function App() {
   return (
       <Routes>
           <Route path="/" element={<Main/>} >
               <Route path="/" element={<Home/>} />
+              <Route path="/laptops" element={<Laptops/>} />
           </Route>
 			</Routes>
   );
---
+++ src/Pages/Filter/Filter.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Laptops from "./../../Conponents/Laptops/Laptops";
+
+
+
+function Filter() {
+    return (
+       <Laptops/>
+    )
+}
+
+export default Filter;
